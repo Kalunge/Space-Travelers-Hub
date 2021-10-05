9005beba55c6119816357d7684528afde58f627e
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-05 14:14:58 +0100

    Fix errors in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,33 +5,32 @@ import logo from '../images/planet.png';
 const Header = () => (
   <>
     <div className="d-flex justify-content-between pt-3 align-items-center p-3">
-  <div className="mb-5">
-    <div className="d-flex justify-content-between pt-3 align-items-center pl-3 mb-5">
-      <ul className="nav">
-        <li className="nav-item">
-          <img src={logo} alt="logo" className="navLogo" />
-        </li>
-        <li className="nav-item">
-          <NavLink exact to="/" activeClassName="active" className="nav-link link-dark fs-4 fw-bolder">
-            Space Travellers Hub
-          </NavLink>
-        </li>
-      </ul>
-      <ul className="nav">
-        <li className="nav-item">
-          <NavLink to="/rockets" activeClassName="active" className="nav-link">
-            Rockets
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/profile" activeClassName="active" className="nav-link">
-            My profile
-          </NavLink>
-        </li>
-      </ul>
+      <div className="d-flex justify-content-between pt-3 align-items-center pl-3 mb-5">
+        <ul className="nav">
+          <li className="nav-item">
+            <img src={logo} alt="logo" className="navLogo" />
+          </li>
+          <li className="nav-item">
+            <NavLink exact to="/" activeClassName="active" className="nav-link link-dark fs-4 fw-bolder">
+              Space Travellers Hub
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="nav">
+          <li className="nav-item">
+            <NavLink to="/rockets" activeClassName="active" className="nav-link">
+              Rockets
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/profile" activeClassName="active" className="nav-link">
+              My profile
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div className="header">{}</div>
-  </div>
+  </>
 );
 
 export default Header;
