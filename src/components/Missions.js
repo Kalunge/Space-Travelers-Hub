c1a3ef581fa6faf452060459cc0d0797d2229fbc
kalunge <muthomititus+github@gmail.com>
2021-10-05 16:01:08 +0100

    Render parts of components based on conditions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,20 +1,31 @@
 /* eslint-disable operator-linebreak */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMissions } from '../redux/missions/missions';
+import {
+  bookMission,
+  getMissions,
+  leaveMission,
+} from '../redux/missions/missions';
 
 const Missions = () => {
-  const missions = useSelector((state) => state.missions);
+  const { missions } = useSelector((state) => state.missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getMissions());
   }, [dispatch]);
 
+  const joinMembershipHandler = (id) => {
+    dispatch(bookMission(id));
+  };
+
+  const leaveMissionHandler = (id) => {
+    dispatch(leaveMission(id));
+  };
+
   return (
     <div className="mt-4 ">
       <table className="table table-bordered table-responsive">
-        git
         <thead>
           <tr>
             <th scope="col">Mission</th>
@@ -24,21 +35,46 @@ const Missions = () => {
           </tr>
         </thead>
         <tbody>
-          {missions[0] &&
-            missions[0].map((item) => (
-              <tr key={item.mission_id}>
-                <th scope="row">{item.mission_name}</th>
+          {missions &&
+            missions.map((item) => (
+              <tr key={item.id}>
+                <th scope="row">{item.name}</th>
                 <td>{item.description}</td>
-                <td>
-                  <button type="button" className="member-btn btn-primary">
-                    ACTIVE MEMBER
-                  </button>
-                </td>
-                <td>
-                  <button type="button" className="member-btn btn-danger">
-                    LEAVE MISSION
-                  </button>
-                </td>
+                {item.reserved ? (
+                  <>
+                    <td>
+                      <button type="button" className="member-btn btn-primary">
+                        ACTIVE MEMBER
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => leaveMissionHandler(item.id)}
+                        type="button"
+                        className="member-btn btn-danger"
+                      >
+                        LEAVE MISSION
+                      </button>
+                    </td>
+                  </>
+                ) : (
+                  <>
+                    <td>
+                      <button type="button" className="member-btn btn-dark">
+                        NOT A MEMBER
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => joinMembershipHandler(item.id)}
+                        type="button"
+                        className="member-btn btn-light"
+                      >
+                        JOIN MISSION
+                      </button>
+                    </td>
+                  </>
+                )}
               </tr>
             ))}
         </tbody>
