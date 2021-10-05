315b265d6d9e49a1316df1378f9e338fa7629cfb
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-05 14:03:10 +0100

    Add rockets page

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Rockets = () => (
-  <div />
+  <div className="d-flex container align-items-center">
+    <img alt="flickr_images" />
+    <div className="p-3">
+      <h3>rocket_name</h3>
+      <p>Rocket Description</p>
+      <button className="btn-primary" type="submit">Reserve Rocket</button>
+    </div>
+  </div>
 );
 
 export default Rockets;
