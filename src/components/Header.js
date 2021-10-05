da9d60dd2c9be0a54b7ec0bdee38b1a51343d957
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-05 11:48:04 +0100

    Add active class name to links

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,24 +10,24 @@ const Header = () => (
           <img src={logo} alt="logo" className="navLogo" />
         </li>
         <li className="nav-item">
-          <NavLink exact to="/" className="nav-link link-dark fs-4 fw-bolder">
+          <NavLink exact to="/" activeClassName="active" className="nav-link link-dark fs-4 fw-bolder">
             Space Travellers Hub
           </NavLink>
         </li>
       </ul>
       <ul className="nav">
         <li className="nav-item">
-          <NavLink to="/rockets" className="nav-link">
+          <NavLink to="/rockets" activeClassName="active" className="nav-link">
             Rockets
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/" className="nav-link border-end">
+          <NavLink to="/" activeClassName="active" className="nav-link border-end">
             Mission
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/profile" className="nav-link">
+          <NavLink to="/profile" activeClassName="active" className="nav-link">
             My profile
           </NavLink>
         </li>
