30cbf768a747f706ff0143ff4c745e9927e7b41f
kalunge <muthomititus+github@gmail.com>
2021-10-05 16:22:12 +0100

    Add reserved missions to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <>
-    <div>
-      <h1>My Missions</h1>
-      <p>Telstar</p>
+const Profile = () => {
+  const { missions } = useSelector((state) => state.missions);
+
+  return (
+    <div className="profile">
+      <div className="missions-profile">
+        <h3>My Missions</h3>
+        {missions.map((mission) => mission.reserved && <p>{mission.name}</p>)}
+      </div>
+      <div className="rockets-profile">
+        <h3>My Rockets</h3>
+        <p>Falcon</p>
+      </div>
     </div>
-    <div>
-      <h1>My Rockets</h1>
-      <p>Falcon</p>
-    </div>
-  </>
-);
+  );
+};
 
 export default Profile;
