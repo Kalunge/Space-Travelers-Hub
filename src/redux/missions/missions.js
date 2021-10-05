c2aafd2142b798a76c3825d41b7e410c3f3d4a7e
kalunge <muthomititus+github@gmail.com>
2021-10-05 14:05:22 +0100

    Add mission leaving functionality

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -4,6 +4,7 @@ const GET_MISSONS = 'spacetravel/missions/GET_MISSIONS';
 // const MISSIONS_REQUEST = 'spacetravel/missions/MISSIONS_REQUEST';
 // const MISSIONS_SUCCESS = 'spacetravel/missions/MISSIONS_SUCCESS';
 const BOOK_MISSION = 'spacetravel/missions/BOOK_MISSION';
+const LEAVE_MISSION = 'spacetravel/missions/LEAVE_MISSION';
 
 export const getMissions = () => async (dispatch) => {
   const { data } = await axios.get('https://api.spacexdata.com/v3/missions');
@@ -13,6 +14,10 @@ export const getMissions = () => async (dispatch) => {
 
 export const bookMission = (id) => (dispatch) => {
   dispatch({ type: BOOK_MISSION, payload: id });
+};
+
+export const leaveMission = (id) => (dispatch) => {
+  dispatch({ type: LEAVE_MISSION, payload: id });
 };
 
 const initialState = [];
@@ -27,6 +32,14 @@ const missions = (state = initialState, action) => {
           return mission;
         }
         return { ...mission, reserved: true };
+      });
+
+    case BOOK_MISSION:
+      return state.filter((mission) => {
+        if (mission.mission_id !== payload) {
+          return mission;
+        }
+        return { ...mission, reserved: false };
       });
     default:
       return state;
