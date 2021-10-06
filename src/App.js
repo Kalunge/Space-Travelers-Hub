6bc8d46a36664c13b4acb93b8bf04127a8f8c5c0
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-06 09:31:29 +0100

    Add dispatch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Profile from './components/Profile';
 import Home from './components/Home';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
+import { getRockets } from './redux/rockets/rockets';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
   return (
     <Router>
       <Header />
