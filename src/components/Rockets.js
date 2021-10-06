77dfd94575a4e347b4918068bb86c34e89336135
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-06 09:33:11 +0100

    display fetched data

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Rockets = () => (
-  <div className="d-flex container align-items-center">
-    <img alt="flickr_images" />
-    <div className="p-3">
-      <h3>rocket_name</h3>
-      <p>Rocket Description</p>
-      <button className="btn-primary" type="submit">Reserve Rocket</button>
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
+  return (
+    <div>
+      {rockets.map((rocket) => (
+        <div key={rocket.rocket_id}>
+          {rocket.rocket_name }
+          {rocket.description }
+          {rocket.flickr_images }
+        </div>
+      ))}
     </div>
-  </div>
-);
-
+  );
+};
 export default Rockets;
