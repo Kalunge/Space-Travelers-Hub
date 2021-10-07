ccb182b2319fe1a77780cf37f30ce0c952eec8f1
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-07 12:31:19 +0100

    Fix linter errors

--- src/components/SingleRockets.js
+++ src/components/SingleRockets.js
@@ -1,15 +1,9 @@
 /* eslint-disable react/prop-types */
-import { useDispatch } from 'react-redux';
-import {
-  cancelRocket,
-  reserveRocket,
-} from '../redux/rockets/rockets';
+import { useDispatch } from "react-redux";
+import { cancelRocket, reserveRocket } from "../redux/rockets/rockets";
 
 const SingleRockets = ({ rocket }) => {
   const dispatch = useDispatch();
-
-
-
   const handleReservation = (id) => {
     dispatch(reserveRocket(id));
   };
@@ -21,30 +15,30 @@ const SingleRockets = ({ rocket }) => {
     <div className="d-flex p-3 w-75" key={rocket.id}>
       <img className="img-rockets" src={rocket.image} alt="rocket_images" />
       <div className="p-3">
-      <h3>{rocket.name}</h3>
-      <p>
-        {rocket.reserved && (
-          <span className="badge bg-secondary">Reserved</span>
+        <h3>{rocket.name}</h3>
+        <p>
+          {rocket.reserved && (
+            <span className="badge bg-secondary">Reserved</span>
+          )}
+          {rocket.description}
+        </p>
+        {rocket.reserved ? (
+          <button
+            type="button"
+            className="btn-light"
+            onClick={() => handleCancellation(rocket.id)}
+          >
+            Cancel Reservation
+          </button>
+        ) : (
+          <button
+            onClick={() => handleReservation(rocket.id)}
+            type="button"
+            className="btn-primary"
+          >
+            Reserve Rocket
+          </button>
         )}
-        {rocket.description}
-      </p>
-      {rocket.reserved ? (
-        <button
-          type="button"
-          className="btn-light"
-          onClick={() => handleCancellation(rocket.id)}
-        >
-          Cancel Reservation
-        </button>
-      ) : (
-        <button
-          onClick={() => handleReservation(rocket.id)}
-          type="button"
-          className="btn-primary"
-        >
-          Reserve Rocket
-        </button>
-      )}
       </div>
     </div>
   );
